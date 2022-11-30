0f6b79a340d5b5d3f414edb8a88057650082d46f
TermiSenpai <escarpaprieto@gmail.com>
2022-11-30 11:29:12 +0100

    Enemy HP increase on round

--- TowerDefense_Alejandro_Escarpa/Assets/Imports/TowerDefence_Vsquad/Scripts/EnemyHp.cs
+++ TowerDefense_Alejandro_Escarpa/Assets/Imports/TowerDefence_Vsquad/Scripts/EnemyHp.cs
@@ -7,6 +7,14 @@ public class EnemyHp : MonoBehaviour
     [SerializeField] EnemyDead enemyDead;
     public int EnemyHP = 30;
 
+    private EnemyWaveSpawner wave;
+    private void Start()
+    {
+        wave = FindObjectOfType<EnemyWaveSpawner>();
+        if (wave.GetRound() > 1)
+            EnemyHP = Mathf.RoundToInt(EnemyHP + wave.GetRound() * 1.5f);
+    }
+
     public void Dmg(int DMGcount)
     {
         EnemyHP -= DMGcount;
