673a08189168660c443b79b4ac520235c772807b
TermiSenpai <escarpaprieto@gmail.com>
2022-12-08 11:18:34 +0100

    HP multiplier

--- TowerDefense_Alejandro_Escarpa/Assets/Scripts/Enemy/EnemyHp.cs
+++ TowerDefense_Alejandro_Escarpa/Assets/Scripts/Enemy/EnemyHp.cs
@@ -6,13 +6,14 @@ public class EnemyHp : MonoBehaviour
 {
     [SerializeField] EnemyDead enemyDead;
     public int EnemyHP = 30;
+    [SerializeField, Range(1f, 5f)] private float hpMultiply = 1.5f;
 
     private EnemyWaveSpawner wave;
     private void Start()
     {
         wave = FindObjectOfType<EnemyWaveSpawner>();
         if (wave.GetRound() > 1)
-            EnemyHP = Mathf.RoundToInt(EnemyHP + wave.GetRound() * 1.5f);
+            EnemyHP = Mathf.RoundToInt(EnemyHP + wave.GetRound() * hpMultiply);
     }
 
     public void Dmg(int DMGcount)
