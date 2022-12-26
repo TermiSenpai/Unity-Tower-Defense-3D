7077f9cd5f515aa82c4313283fff3a65f40714bb
TermiSenpai <escarpaprieto@gmail.com>
2022-12-26 13:59:43 +0100

    free camera movement

--- TowerDefense_Alejandro_Escarpa/Assets/Scripts/Manager/CameraMovement.cs
+++ TowerDefense_Alejandro_Escarpa/Assets/Scripts/Manager/CameraMovement.cs
@@ -4,51 +4,182 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    private bool canMove = true;
 
-    [Header("Padding")]
-    [SerializeField] private float panSpeed;
+    #region old
+    //private bool canMove = true;
 
-    [Header("Scrollinig")]
-    [SerializeField] private float multiplier;
-    [SerializeField] private float scrollSpeed;
-    [SerializeField] private float minY;
-    [SerializeField] private float maxY;
+    //[Header("Padding")]
+    //[SerializeField] private float panSpeed;
+
+    //[Header("Scrollinig")]
+    //[SerializeField] private float multiplier;
+    //[SerializeField] private float scrollSpeed;
+    //[SerializeField] private float minY;
+    //[SerializeField] private float maxY;
 
 
-    private void Update()
+    //private void Update()
+    //{
+    //    if (!canMove)
+    //        return;
+
+    //    InputCamera();
+    //    ScrollCamera();
+    //}
+
+    //private void InputCamera()
+    //{
+    //    if (Input.GetKey(KeyCode.W))
+    //        moveCamera(Vector3.forward);
+    //    if (Input.GetKey(KeyCode.S))
+    //        moveCamera(Vector3.back);
+    //    if (Input.GetKey(KeyCode.D))
+    //        moveCamera(Vector3.right);
+    //    if (Input.GetKey(KeyCode.A))
+    //        moveCamera(Vector3.left);
+    //}
+
+    //private void ScrollCamera()
+    //{
+    //    float scroll = Input.GetAxis("Mouse ScrollWheel");
+    //    Vector3 pos = transform.position;
+    //    pos.y -= scroll * multiplier * scrollSpeed * Time.deltaTime;
+    //    pos.y = Mathf.Clamp(pos.y, minY, maxY);
+
+    //    transform.position = pos;
+    //}
+
+    //private void moveCamera(Vector3 dir)
+    //{
+    //    transform.Translate(dir * panSpeed * Time.deltaTime, Space.World);
+    //}
+    #endregion
+
+    #region New
+    /// <summary>
+    /// Normal speed of camera movement.
+    /// </summary>
+    public float movementSpeed = 10f;
+
+    /// <summary>
+    /// Speed of camera movement when shift is held down,
+    /// </summary>
+    public float fastMovementSpeed = 100f;
+
+    /// <summary>
+    /// Sensitivity for free look.
+    /// </summary>
+    public float freeLookSensitivity = 3f;
+
+    /// <summary>
+    /// Amount to zoom the camera when using the mouse wheel.
+    /// </summary>
+    public float zoomSensitivity = 10f;
+
+    /// <summary>
+    /// Amount to zoom the camera when using the mouse wheel (fast mode).
+    /// </summary>
+    public float fastZoomSensitivity = 50f;
+
+    /// <summary>
+    /// Set to true when free looking (on right mouse button).
+    /// </summary>
+    private bool looking = false;
+
+    void Update()
     {
-        if (!canMove)
-            return;
+        var fastMode = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+        var movementSpeed = fastMode ? this.fastMovementSpeed : this.movementSpeed;
 
-        InputCamera();
-        ScrollCamera();
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position = transform.position + (-transform.right * movementSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position = transform.position + (transform.right * movementSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position = transform.position + (transform.forward * movementSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position = transform.position + (-transform.forward * movementSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.position = transform.position + (transform.up * movementSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.position = transform.position + (-transform.up * movementSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.R) || Input.GetKey(KeyCode.PageUp))
+        {
+            transform.position = transform.position + (Vector3.up * movementSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.F) || Input.GetKey(KeyCode.PageDown))
+        {
+            transform.position = transform.position + (-Vector3.up * movementSpeed * Time.deltaTime);
+        }
+
+        if (looking)
+        {
+            float newRotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * freeLookSensitivity;
+            float newRotationY = transform.localEulerAngles.x - Input.GetAxis("Mouse Y") * freeLookSensitivity;
+            transform.localEulerAngles = new Vector3(newRotationY, newRotationX, 0f);
+        }
+
+        float axis = Input.GetAxis("Mouse ScrollWheel");
+        if (axis != 0)
+        {
+            var zoomSensitivity = fastMode ? this.fastZoomSensitivity : this.zoomSensitivity;
+            transform.position = transform.position + transform.forward * axis * zoomSensitivity;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            StartLooking();
+        }
+        else if (Input.GetKeyUp(KeyCode.Mouse1))
+        {
+            StopLooking();
+        }
+
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, 2.5f, 200f), transform.position.z);
     }
 
-    private void InputCamera()
+    void OnDisable()
     {
-        if (Input.GetKey(KeyCode.W))
-            moveCamera(Vector3.forward);
-        if (Input.GetKey(KeyCode.S))
-            moveCamera(Vector3.back);
-        if (Input.GetKey(KeyCode.D))
-            moveCamera(Vector3.right);
-        if (Input.GetKey(KeyCode.A))
-            moveCamera(Vector3.left);
+        StopLooking();
     }
 
-    private void ScrollCamera()
+    /// <summary>
+    /// Enable free looking.
+    /// </summary>
+    public void StartLooking()
     {
-        float scroll = Input.GetAxis("Mouse ScrollWheel");
-        Vector3 pos = transform.position;
-        pos.y -= scroll * multiplier * scrollSpeed * Time.deltaTime;
-        pos.y = Mathf.Clamp(pos.y, minY, maxY);
-
-        transform.position = pos;
+        looking = true;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    private void moveCamera(Vector3 dir)
+    /// <summary>
+    /// Disable free looking.
+    /// </summary>
+    public void StopLooking()
     {
-        transform.Translate(dir * panSpeed * Time.deltaTime, Space.World);
+        looking = false;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
     }
+    #endregion
 }
