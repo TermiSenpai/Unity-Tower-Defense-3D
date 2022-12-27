2f086faa79a61907419fd9c471abcd48c0070f18
TermiSenpai <escarpaprieto@gmail.com>
2022-12-27 12:21:07 +0100

    UI sounds

--- TowerDefense_Alejandro_Escarpa/Assets/Scripts/UI/Shop.cs
+++ TowerDefense_Alejandro_Escarpa/Assets/Scripts/UI/Shop.cs
@@ -5,13 +5,23 @@ using UnityEngine;
 
 public class Shop : MonoBehaviour
 {
+    [SerializeField] UISoundsManager UI_SoundsManager;
+    [SerializeField] UIPanelsManager UI_PanelsManager;
+    [SerializeField] AudioClip errorSound;
+    [SerializeField] AudioClip BuySound;
 
     public void PurchaseTurret(Turret turret)
     {
         if (Currency.Money >= turret.cost)
         {
             BuildManager.instance.SetTurretToBuild(turret);
+            UI_PanelsManager.ClosePanel();
+            UI_SoundsManager.PlayUISound(BuySound);
             Debug.Log($"The {turret} has been purchased");
+        }
+        else
+        {
+            UI_SoundsManager.PlayUISound(errorSound);
         }
     }
 }
